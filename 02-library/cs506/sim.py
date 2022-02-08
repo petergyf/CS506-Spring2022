d20e3ff5bda56a2a7db0eb216abed0c8c270015b
petergyf <guoyf1@shanghaitech.edu.cn>
2022-02-08 13:22:45 -0500

    finish read and sim

--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def euclidean_dist(x, y):
     res = 0
     for i in range(len(x)):
@@ -5,12 +7,25 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    res = 0
+    for i in range(len(x)):
+        res += abs(x[i] - y[i])
+    return res
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    if len(x) == 0 and len(y) == 0:
+        return "lengths must not be zero"
+    a = set(x)
+    b = set(y)
+    # calucate jaccard similarity
+    j = 1 - float(len(a.intersection(b))) / len(a.union(b))
+    return j
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    if len(x) == 0 or len(y) == 0:
+        return "lengths must not be zero"
+    if len(x) != len(y):
+        return "lengths must be equal"
+    return np.dot(x, y)/(np.linalg.norm(x)*np.linalg.norm(y))
 
 # Feel free to add more
